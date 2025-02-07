92304bd87eb6f3dfc4d96e3ecb807c0d1d081834
carlosGomezMojica <gomezmojicacarlosmanuel@gmail.com>
2025-02-07 11:08:21 -0500

    configuracion de nvim con lazyvim

---
+++ nvim/lua/plugins/test.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "airblade/vim-gitgutter",
+    config = function() end,
+  },
+  { "tpope/vim-fugitive" },
+}
---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "moon",
+      transparent = true, -- Habilita transparencia
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight-moon")
+    end,
+  },
+}
